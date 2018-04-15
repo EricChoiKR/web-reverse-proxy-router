6dcbe0fc1cb44f1c08ecb5611de3066d4d7f4938
Seongguk Choi <seonggukchoi@Seonggukui-MacBook-Pro.local>
2018-04-15 20:43:31 +0900

    Test

--- app.ts
+++ app.ts
@@ -1,11 +1,23 @@
 import * as express from "express";
+import * as subdomain from "express-subdomain";
 import * as config from "config";
 
 const server = express();
 
+const blog_router = express.Router();
+
 server.get("/", (req, res) => {
+  console.log("/");
   res.send("OK");
 });
+
+// Blog
+blog_router.get("/", (req, res) => {
+  console.log("blog");
+  res.send("Blog");
+});
+
+server.use(subdomain("blog", blog_router));
 
 server.listen(config.get("port"), (req, res, error) => {
   if (error) {
